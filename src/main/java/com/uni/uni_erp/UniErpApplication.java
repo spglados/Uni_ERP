a373c2a475d7a23005778a64a65396400f635437
spglados <spglados@gmail.com>
2024-10-08 18:02:44 +0900

    first commit

---
+++ src/main/java/com/uni/uni_erp/UniErpApplication.java
@@ -0,0 +1,13 @@
+package com.uni.uni_erp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniErpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniErpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uni/uni_erp/UniErpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uni.uni_erp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniErpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
