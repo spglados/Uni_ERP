cc23182ed6510a41637575e1a4f996bc8caa1e80
kyeonghooon <gibbon0510@gmail.com>
2024-10-13 01:28:13 +0900

    :sparkles: 구글 클라우드 비전 테스트

---
+++ src/main/java/com/uni/uni_erp/util/OCRTest.java
@@ -0,0 +1,57 @@
+package com.uni.uni_erp.util;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.uni.uni_erp.util.ocrDTO.ApiResponse;
+import org.springframework.http.*;
+import org.springframework.web.client.RestTemplate;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Base64;
+import java.util.List;
+
+public class OCRTest {
+
+    private static final String API_URL = "https://vision.googleapis.com/v1/images:annotate?key=";
+
+    public static void main(String[] args) {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            Path imagePath = Paths.get("c://image/20241010_173941.jpg");
+
+            byte[] imageBytes = Files.readAllBytes(imagePath);
+            String base64Image = Base64.getEncoder().encodeToString(imageBytes);
+
+            String requestBody = "{ \"requests\": [{ \"image\": { \"content\": \"" + base64Image + "\" }, " +
+                    "\"features\": [{ \"type\": \"TEXT_DETECTION\" }], " +
+                    "\"imageContext\": { \"languageHints\": [\"ko\", \"en\"] } }] }";
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+
+            // 5. 요청 본문과 헤더를 HttpEntity에 담기
+            HttpEntity<String> entity = new HttpEntity<>(requestBody, headers);
+
+            // 6. RestTemplate을 사용하여 API 요청 보내기
+            RestTemplate restTemplate = new RestTemplate();
+            ResponseEntity<ApiResponse> response = restTemplate.exchange(API_URL, HttpMethod.POST, entity, ApiResponse.class);
+            List<ApiResponse.Responses.TextAnnotation> list = response.getBody().getResponses().get(0).getTextAnnotations();
+            System.out.println("리스트 길이 : " + list.size());
+            for (int i = 1; i < list.size(); i++) {
+                ApiResponse.Responses.TextAnnotation textAnnotation = list.get(i);
+                String description = textAnnotation.getDescription();
+                System.out.println("텍스트 : " + description);
+                List<ApiResponse.Responses.TextAnnotation.BoundingPoly.Vertice> xy = textAnnotation.getBoundingPoly().getVertices();
+                StringBuilder sb = new StringBuilder();
+                for (int j = 0; j < xy.size(); j++) {
+                    sb.append("(").append(xy.get(j).getX()).append(",").append(xy.get(j).getY()).append(")");
+                }
+                System.out.println(sb.toString());
+            }
+            // 7. API 응답 출력
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/com/uni/uni_erp/util/ocrDTO/ApiResponse.java
@@ -0,0 +1,32 @@
+package com.uni.uni_erp.util.ocrDTO;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class ApiResponse {
+    private List<Responses> responses;
+
+    @Data
+    public static class Responses {
+        private List<TextAnnotation> textAnnotations;
+
+        @Data
+        public static class TextAnnotation {
+            private String description;
+            private BoundingPoly boundingPoly;
+
+            @Data
+            public static class BoundingPoly {
+                private List<Vertice> vertices;
+
+                @Data
+                public static class Vertice {
+                    int x;
+                    int y;
+                }
+            }
+        }
+    }
+}
