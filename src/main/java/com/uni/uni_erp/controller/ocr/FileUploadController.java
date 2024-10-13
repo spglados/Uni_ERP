04f64ea52766d75fbcfe55a3f7e78ad7f095b04c
SPGLADOS <spglados@gmail.com>
2024-10-14 01:15:09 +0900

    :hammer: 이미지업로드 페이지 구현중

---
+++ src/main/java/com/uni/uni_erp/controller/ocr/FileUploadController.java
@@ -0,0 +1,24 @@
+package com.uni.uni_erp.controller.ocr;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/file")
+public class FileUploadController {
+
+    @GetMapping("/test")
+    public String fileUploadTestPage(){
+        return "/model/image_upload";
+    }
+
+    @PostMapping("/test/upload")
+    public void fileUploadTestPost() {
+
+
+    }
+
+
+}
