2f02a208c7f72484d9705f0962cad1099a39b253
nambanote <158823905+Nam-Cheol@users.noreply.github.com>
2024-10-14 16:17:23 +0900

    :sparkles: Store Id Add

--- src/main/java/com/uni/uni_erp/repository/product/IngredientRepository.java
+++ src/main/java/com/uni/uni_erp/repository/product/IngredientRepository.java
@@ -2,6 +2,7 @@ package com.uni.uni_erp.repository.product;
 
 import com.uni.uni_erp.domain.entity.erp.product.Ingredient;
 import org.hibernate.type.descriptor.converter.spi.JpaAttributeConverter;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface IngredientRepository extends JpaAttributeConverter<Ingredient, Integer> {
+public interface IngredientRepository extends JpaRepository<Ingredient, Integer> {
 }
