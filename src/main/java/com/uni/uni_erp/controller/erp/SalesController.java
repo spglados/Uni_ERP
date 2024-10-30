3301c5c2dc6fdfc72d70144a1402178d7b02c172
ryipuxt <167273832+ryipuxt@users.noreply.github.com>
2024-10-30 11:05:04 +0900

    임시저장
    * 테이블, 컨트롤러 임시 저장
    
    * 테이블, 컨트롤러 임시 저장
    
    * 데이터 받아오기 성공
    차트만들어야됨
    
    * 차트 임시
    
    * 차트
    
    * 차트
    
    * 차트 / 임시 데이터
    
    * 분석 메인화면
    
    * :sparkles: 매출 통계 상세 페이지 컨트롤러 / 서비스 구현
    
    * :sparkles: 매출 통계 상세 페이지 구현
    
    * :bug: jsp 페이지 충돌 수정
    
    * :fire: 필요없는 로깅 삭제
    
    * :fire: 필요없는 로깅 삭제
    
    * 임시 저장
    
    * :sparkles: 일별 차트 추가
    
    * :card_file_box: 샘플 데이터 추가
    
    * :card_file_box: 샘플 데이터 추가
    
    * :sparkles: 상세보기 테이블 수정
    
    * :recycle: ajax > fetch 변경
    
    * :sparkles: 세일 데이터 insert
    
    * :sparkles: pos > 세일 데이터 db에 추가
    
    * :card_file_box: 샘플데이터 변경
    
    * :sparkles: pos > 세일 데이터 db에 추가
    
    * :fire: 필요없는 jsp 삭제
    
    * :bug: 버튼 작동하게 수정
    
    * 임시 저장
    
    * 임시 저장
    
    * :sparkles: SalesDetail 엔티티 변경,
    SalesRefundDetail 엔티티 추가
    
    * :card_file_box: 컬럼 이름 변경
    
    * :sparkles: 환불tb 세이브 메서드 추가
    
    * :truck: DTO 이름 변경
    
    * :card_file_box: 샘플 데이터 수정
    
    * :sparkles: 주문내역 불러오기 / 환불처리
    
    * :sparkles: 인당코스트 / 환불취소 내역 임시저장
    
    * 임시저장
    
    ---------
    
    Co-authored-by: nambanote <158823905+Nam-Cheol@users.noreply.github.com>

--- src/main/java/com/uni/uni_erp/controller/erp/SalesController.java
+++ src/main/java/com/uni/uni_erp/controller/erp/SalesController.java
@@ -1,5 +1,6 @@
 package com.uni.uni_erp.controller.erp;
 
+import com.google.gson.Gson;
 import com.uni.uni_erp.domain.entity.User;
 import com.uni.uni_erp.dto.CostPerEmployeeDTO;
 import com.uni.uni_erp.dto.erp.hr.EmployeeDTO;
@@ -11,6 +12,7 @@ import com.uni.uni_erp.service.erp.hr.HrService;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.json.JSONArray;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -48,21 +50,7 @@ public class SalesController {
     }
 
     @GetMapping("/statistics")
-    public String salesStatistics(Model model, HttpSession session) {
-
-        session.getAttribute("storeId");
-
-        LocalDate today = LocalDate.now();
-        LocalDate yesterday = LocalDate.now().minusDays(1);
-        LocalDate lastYear = LocalDate.now().minusYears(1);
-
-        List<SalesTargetDTO> todaySalesTargets = salesService.getSalesTargetByHour(1, today);
-        List<SalesTargetDTO> yesterdaySalesTargets = salesService.getSalesTargetByHour(1, yesterday);
-        List<SalesTargetDTO> lastYearSalesTargets = salesService.getSalesTargetByHour(1, lastYear);
-
-        List<SalesComparisonDTO> salesComparison = salesService.getSalesComparison(todaySalesTargets, yesterdaySalesTargets, lastYearSalesTargets);
-
-        model.addAttribute("salesComparison", salesComparison);
+    public String salesStatistics() {
 
         return "erp/sales/statistics";
     }
