633c190a955ff729e5cce9d655036e57fe24c899
nambanote <158823905+Nam-Cheol@users.noreply.github.com>
2024-11-03 13:37:58 +0900

    :wrench: 버그 수정

--- src/main/java/com/uni/uni_erp/repository/erp/inventory/MaterialRepository.java
+++ src/main/java/com/uni/uni_erp/repository/erp/inventory/MaterialRepository.java
@@ -18,6 +18,7 @@ public interface MaterialRepository extends JpaRepository<Material, Integer> {
     @Query("SELECT new com.uni.uni_erp.dto.erp.material.MaterialDTO(m.id, m.name, m.unit, m.subUnit) FROM Material m WHERE m.store.id = :storeId")
     List<MaterialDTO> findDTOByStoreId(@Param("storeId") Integer storeId);
 
+    @Query("SELECT m FROM Material m WHERE m.store.id = :storeId")
     List<Material> findAllByStoreId(Integer storeId);
 
     @Query("SELECT m FROM Material m WHERE m.materialCode IN :materialCodes AND m.store.id = :storeId")
